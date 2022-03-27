561252e7b15ee2aef137aa835d61bf3d93262575
AAShah <megananospeed@gmail.com>
2022-03-27 20:08:57 +0300

    CyberOps Website
    ...

---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'Challanges',
+    path: '/challanges',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Workshops',
+    path: '/workshops',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Events',
+    path: '/events',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Competitions',
+    path: '/competitions',
+    cName: 'dropdown-link'
+  }
+];
