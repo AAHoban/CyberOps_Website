3daf8c822b2e39e2838e58d0764b1eefda6796f6
Ali AlHoban <megananospeed@gmail.com>
2023-07-18 16:03:43 +0300

    Cyberops Website
    Removed disable react devtools

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://technotes-api.onrender.com'
+    baseURL: 'https://cyberops-website-api.onrender.com'
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import './index.scss';
-
-if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
