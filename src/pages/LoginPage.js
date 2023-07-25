1fcb3aeab77d14f87bc57d3e2a7f7fda5311fe5e
Ali AlHoban <megananospeed@gmail.com>
2023-07-25 10:22:26 +0300

    Cyberops-Website

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React, {useContext, useState} from "react";
 import {Navigate} from "react-router-dom";
-import {UserContext} from "../context/UserContext";
+import {UserContext} from "../App";
 import '../css/Login.scss';
 
 export default function LoginPage() {
