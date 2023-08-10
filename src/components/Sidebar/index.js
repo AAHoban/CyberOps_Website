a57d90eb242e2792c6e0a96fb578716781547af5
Ali AlHoban <megananospeed@gmail.com>
2023-08-10 12:08:16 +0300

    Cyberops-Website
    Theme mode Icons changed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -93,8 +93,8 @@ const Sidebar = (props) => {
                   <svg
                     viewBox="0 0 24 24"
                     fill="currentColor"
-                    height="1.7rem"
-                    width="1.7rem"
+                    height="1.5rem"
+                    width="1.5rem"
                     {...props}
                   >
                     <path d="M12 11.807A9.002 9.002 0 0110.049 2a9.942 9.942 0 00-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 002.735-5.119A9.003 9.003 0 0112 11.807z" />
@@ -182,12 +182,29 @@ const Sidebar = (props) => {
       <div className="toggle-btn-section-mobile">
       <div className={`toggle-checkbox m-vertical-auto`}>
             <button type="button" className={`toggle-btn__input-label`} onClick={handleThemeChange}>
-              <button className='menuIcons'>
+            <button className='menuIcons'>
                 {theme === 'dark' ? 
-                  <h1>Moon</h1> 
-                : 
-                  <h1>Sun</h1>}
-                </button>
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    height="2rem"
+                    width="2rem"
+                    {...props}
+                  >
+                    <path d="M12 11.807A9.002 9.002 0 0110.049 2a9.942 9.942 0 00-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 002.735-5.119A9.003 9.003 0 0112 11.807z" />
+                  </svg>
+                  :     
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    height="2rem"
+                    width="2rem"
+                    {...props}
+                  >
+                    <path d="M6.995 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007-2.246-5.007-5.007-5.007S6.995 9.239 6.995 12zM11 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2H2zm17 0h3v2h-3zM5.637 19.778l-1.414-1.414 2.121-2.121 1.414 1.414zM16.242 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.344 7.759L4.223 5.637l1.415-1.414 2.12 2.122zm13.434 10.605l-1.414 1.414-2.122-2.122 1.414-1.414z" />
+                  </svg>
+                }
+              </button>
             </button>
           </div>
       </div>
