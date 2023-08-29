d95c430a8a97d11c6c0c5dd39277deb290b43a5e
Ali AlHoban <megananospeed@gmail.com>
2023-08-29 16:55:19 +0300

    Cyberops-Frontend

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -36,7 +36,7 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`https://backend-cyberops.onrender.com/${postInfo.cover}`} alt=""/>
+        <img src={`https://cyberops.onrender.com/api/${postInfo.cover}`} alt="Post Thumbnail"/>
       </div>
       <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
     </div>
